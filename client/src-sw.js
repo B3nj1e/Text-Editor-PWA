78cb987d1b6fb766b836c43197ce8706f0859561
Benjamin Clewer <benjiclewer@gmail.com>
2022-01-21 16:54:45 +1030

    SW

--- client/src-sw.js
+++ client/src-sw.js
@@ -27,7 +27,6 @@ warmStrategyCache({
 
 registerRoute(({ request }) => request.mode === 'navigate', pageCache);
 
-// TODO: Implement asset caching
 // Set up asset cache
 registerRoute(
   // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
@@ -43,3 +42,5 @@ registerRoute(
     ],
   })
 );
+
+registerRoute();
