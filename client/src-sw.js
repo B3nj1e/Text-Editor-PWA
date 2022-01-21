e732c86045395b1d45caa760e86063b5464c0d85
Benjamin Clewer <benjiclewer@gmail.com>
2022-01-21 15:29:59 +1030

    updates to database

--- client/src-sw.js
+++ client/src-sw.js
@@ -4,6 +4,7 @@ const { registerRoute } = require('workbox-routing');
 const { CacheableResponsePlugin } = require('workbox-cacheable-response');
 const { ExpirationPlugin } = require('workbox-expiration');
 const { precacheAndRoute } = require('workbox-precaching/precacheAndRoute');
+const { StaleWhileRevalidate } = require('workbox-strategies');
 
 precacheAndRoute(self.__WB_MANIFEST);
 
@@ -24,7 +25,42 @@ warmStrategyCache({
   strategy: pageCache,
 });
 
-registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+// registerRoute(({ request }) => request.mode === 'navigate', pageCache);
+
+
+// self.addEventListener('install', function(event) {
+//   event.waitUntil(
+//     caches.open(cacheName).then(function(cache) {
+//       return cache.addAll(
+//         [
+//           '/css/bootstrap.css',
+//           '/css/main.css',
+//           '/js/bootstrap.min.js',
+//           '/js/jquery.min.js',
+//           '/offline.html'
+//         ]
+//       );
+//     })
+//   );
+// });
+
+
+registerRoute(
+  // Here we define the callback function that will filter the requests we want to cache (in this case, JS and CSS files)
+  ({ request }) => ['style', 'script', 'worker'].includes(request.destination),
+  new StaleWhileRevalidate({
+    // Name of the cache storage.
+    cacheName: 'asset-cache',
+    plugins: [
+      // This plugin will cache responses with these headers to a maximum-age of 30 days
+      new CacheableResponsePlugin({
+        statuses: [0, 200],
+      }),
+    ],
+  })
+);
+
+
 
 // TODO: Implement asset caching
 registerRoute();
