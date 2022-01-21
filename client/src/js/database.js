2be6b384e2035aba3e7e995cdcddb9261a8dea78
Benjamin Clewer <benjiclewer@gmail.com>
2022-01-21 12:34:01 +1030

    working on database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -17,32 +17,22 @@ initdb();
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  // let request = jate.add(content);
-  // request.onsuccess = function () {
-  //   console.log("note added to the store", request.result);
-  // }
-  
-  // if (!conent) {
-  // console.error('putDb not implemented');
   console.log('PUT to the database');
   const jateDb = await openDB('jate', 1);
-  const tx = jateDb.transaction('jate', 'readwrite');
-  const store = tx.objectStore('jate');
+  const tx = jateDb.transaction('value', 'readwrite');
+  const store = tx.objectStore('value');
   const request = store.put({ id: id, note: content });
   const result = await request;
-  console.log('🚀 - data saved to the database', result);
+  console.log('data saved to the database', result);
 };
 
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  // const value = await db.getAll('jate'); 
-  // if (!value) {
-  // console.error('getDb not implemented');} }
   console.log('GET all from the database');
   const jateDb = await openDB('jate', 1);
-  const note = jateDb.transaction('jate', 'readonly');
-  const store = note.objectStore('jate');
+  const note = jateDb.transaction('value', 'readonly');
+  const store = note.objectStore('value');
   const request = store.getAll();
   const result = await request;
   console.log('result.value', result);
